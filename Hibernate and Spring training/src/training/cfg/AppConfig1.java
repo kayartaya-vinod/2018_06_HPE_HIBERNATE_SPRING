26e20edd867d5449d3f19c9bd7ca66f8b9ffac3c
kayartaya-vinod <kayartaya.vinod@gmail.com>
2018-06-22 08:45:19 +0530

    day 3

--- Hibernate and Spring training/src/training/cfg/AppConfig1.java
+++ Hibernate and Spring training/src/training/cfg/AppConfig1.java
@@ -7,26 +7,10 @@ import training.dao.impl.ProductDaoJdbcImpl;
 
 @Configuration
 public class AppConfig1 {
-	
-	public AppConfig1() {
-		System.out.println("AppConfig1 instantiated!");
-	}
-	
-	@Bean
-	public Integer someNum() {
-		System.out.println("AppConfig1.someNum() called");
-		return 100;
-	}
 
 	@Bean
 	public ProductDaoJdbcImpl dao() {
-		System.out.println("AppConfig1.dao() called");
-		this.someNum();
-		this.someNum();
-		this.someNum();
-		this.someNum();
-		this.someNum();
 		return new ProductDaoJdbcImpl();
 	}
-	
+
 }
